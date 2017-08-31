a17981c73439666ac8401b6c1a3d0bf5da0f8c17
ramesh2901 <rameshkoritikanti@gmail.com>
2017-08-31 14:23:29 -0700

    file is added

---
+++ linearsearch.py
@@ -0,0 +1,16 @@
+def linearsearch(item,list):
+	found = False
+	pos = 0
+	while pos < len(list) and not found:
+		if list[pos] == item:
+			found = True
+		pos = pos+1
+	return found
+if __name__ == "__main__":
+	list = [3,5,79,2,1,5,7,9,12,23,6,8]
+	item = int(input("Enter your number:"))
+	isitfound = linearsearch(item,list)
+	if isitfound:
+		print("your item is in the list")
+	else:
+		print("your item is not in the list")
